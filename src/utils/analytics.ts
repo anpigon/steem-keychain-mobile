bf91959205f4f0409d7a5b006a59fc25c4f1f85d
anpigon <markan82@gmail.com>
2021-07-16 16:57:39 +0900

    update analytics logging try catch

--- src/utils/analytics.ts
+++ src/utils/analytics.ts
@@ -4,15 +4,19 @@ let previousRouteName: string;
 let lastWalletPage = 'WalletScreen';
 
 export const logScreenView = async (routeName: string) => {
-  if (routeName !== previousRouteName) {
-    previousRouteName = routeName;
+  try {
+    if (routeName !== previousRouteName) {
+      previousRouteName = routeName;
 
-    routeName = routeName.replace('Screen', '');
-    console.log(`Logging route : ${routeName}`);
+      routeName = routeName.replace('Screen', '');
+      console.debug(`Logging route : ${routeName}`);
 
-    await analytics().logScreenView({
-      screen_name: routeName,
-      screen_class: routeName,
-    });
+      await analytics().logScreenView({
+        screen_name: routeName,
+        screen_class: routeName,
+      });
+    }
+  } catch (e) {
+    console.error('error analytics', e);
   }
 };
