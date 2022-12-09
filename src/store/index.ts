6b9f638b9a90f02837fdfe1c4d24e35fed9b4321
anpigon <markan82@gmail.com>
2022-12-09 16:26:22 +0900

    update: store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,19 +1,34 @@
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import AsyncStorage from '@react-native-community/async-storage';
 import reducers from 'reducers/index';
 import {applyMiddleware, createStore} from 'redux';
 import {persistReducer, persistStore} from 'redux-persist';
 import thunk from 'redux-thunk';
-import { composeWithDevTools } from 'remote-redux-devtools';
+import {composeWithDevTools} from 'remote-redux-devtools';
+import transforms from './transforms';
 
 const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+  whitelist: [
+    'lastAccount',
+    'settings',
+    'browser',
+    'preferences',
+    'hive_authentication_service',
+  ],
+  transforms,
+};
+const persistConfig2 = {
   key: 'root',
   storage: AsyncStorage,
   whitelist: ['lastAccount', 'settings', 'browser', 'preferences'],
 };
 
 const persistedReducers = persistReducer(persistConfig, reducers);
-const store = createStore(persistedReducers, composeWithDevTools(applyMiddleware(thunk)));
-
+const store = createStore(
+  persistedReducers,
+  composeWithDevTools(applyMiddleware(thunk)),
+);
 const persistor = persistStore(store);
 
 export {store, persistor};
---
+++ src/store/transforms.ts
@@ -0,0 +1,49 @@
+import {Settings} from 'actions/interfaces';
+import {HAS_State} from 'reducers/hiveAuthenticationService';
+import createTransform from 'redux-persist/es/createTransform';
+
+const rpcTransformer = createTransform<Settings, Settings>(
+  (inboundState) => inboundState,
+  (outboundState, key) => {
+    if (key === 'settings' && typeof outboundState.rpc === 'string') {
+      return {
+        ...outboundState,
+        rpc: {uri: outboundState.rpc, testnet: false},
+      } as Settings;
+    }
+
+    return outboundState;
+  },
+  {whitelist: ['settings']},
+);
+
+const hiveAuthenticationServiceTransformer = createTransform<
+  HAS_State,
+  HAS_State
+>(
+  (inboundState, key, state) => {
+    if (state?.auth?.mk) return inboundState;
+    console.log('modifying state');
+    const sessions = inboundState.sessions
+      .filter((e) => e.token && e.token.expiration > Date.now())
+      .map((e) => {
+        e.init = false;
+        return e;
+      });
+    return {
+      instances: inboundState.instances
+        .filter((e) => !!sessions.find((session) => e.host === session.host))
+        .map((e) => {
+          e.init = false;
+          delete e.server_key;
+          delete e.connected;
+          return e;
+        }),
+      sessions,
+    };
+  },
+  (outboundState) => outboundState,
+  {whitelist: ['hive_authentication_service']},
+);
+
+export default [rpcTransformer, hiveAuthenticationServiceTransformer];
