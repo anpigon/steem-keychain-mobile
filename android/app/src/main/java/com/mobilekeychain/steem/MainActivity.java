c21628a7919779f9ec94f1f548eff0e232754852
anpigon <markan82@gmail.com>
2022-12-09 12:23:46 +0900

    chore: android

--- android/app/src/main/java/com/mobilekeychain/steem/MainActivity.java
+++ android/app/src/main/java/com/mobilekeychain/steem/MainActivity.java
@@ -1,6 +1,11 @@
 package com.mobilekeychain.steem;
+import android.os.Bundle; // <- add this necessary import
+import com.zoontek.rnbootsplash.RNBootSplash; // <- add this necessary import
+
 
 import com.facebook.react.ReactActivity;
+import android.content.Intent; // <--- import
+import android.content.res.Configuration; // <--- import
 
 public class MainActivity extends ReactActivity {
 
@@ -11,5 +16,17 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "mobileKeychain";
+  }
+  @Override
+  public void onConfigurationChanged(Configuration newConfig) {
+    super.onConfigurationChanged(newConfig);
+    Intent intent = new Intent("onConfigurationChanged");
+    intent.putExtra("newConfig", newConfig);
+    this.sendBroadcast(intent);
+}
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState); // or super.onCreate(null) with react-native-screens
+    RNBootSplash.init(R.drawable.bootsplash, MainActivity.this); // display the generated bootsplash.xml drawable over our MainActivity
   }
 }
